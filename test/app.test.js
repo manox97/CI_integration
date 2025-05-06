2b55b9cfd984236542f5109f177fc2d2c265fd08
Manohar <manox21@gmail.com>
2025-05-06 11:25:33 +0530

    Pass case CI commit

--- test/app.test.js
+++ test/app.test.js
@@ -5,7 +5,7 @@ describe('GET /', () => {
     it("should return 200 status and the correct message", async() => {
         const response = await request(app).get("/");
         expect(response.status).toBe(200);
-        expect(response.text).toBe(`Welcome to ShopEasy from code  ${name}!`);
+        expect(response.text).toBe(`Welcome to ShopEasy from ${name}!`);
     })
 })
 afterAll(() => {
